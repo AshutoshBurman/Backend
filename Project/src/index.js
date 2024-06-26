23e38bb7f8dc2b0af736e1ee9b5f1de8ff7d5abf
Ashutosh <ashutosh.burman@musicinfocloud.com>
2024-06-26 17:09:47 +0300

    model and JWT token implemented

--- Project/src/index.js
+++ Project/src/index.js
@@ -3,10 +3,22 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { DB_NAME } from './constants.js';
 import connectDB from './db/index.js';
+import express from 'express';
+const app = express();
 
 dotenv.config({ path: './env' });
 
-connectDB(DB_NAME)
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => { // Make sure to pass 'err' as a parameter to catch any errors
+    console.log('MONGO db connection failed !!!', err);
+    console.error(err);
+    throw err;
+});
 
 
 
